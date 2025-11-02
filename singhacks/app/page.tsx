52a2c26d9b60ce8a50f9eae180e9a8d72e12cc30
Sa1ram10 <sairamsg10@gmail.com>
2025-11-02 11:05:17 +0800

    redirect to dashboard

--- singhacks/app/page.tsx
+++ singhacks/app/page.tsx
@@ -1,9 +1,5 @@
-import HeroSectionOne from "@/components/ui/hero-section";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="bg-background">
-      <HeroSectionOne />
-    </main>
-  );
+  redirect("/dashboard");
 }
